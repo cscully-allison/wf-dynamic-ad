9bca29fd77e73b53446dd7e62135bba6f2af1567
cscully-allison <cscully-allison@nevada.unr.edu>
2017-09-20 21:18:11 -0700

    Interface working with demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  offer_classes = [1, 2, 3, 4, 5, 6];
+  selectedClass: number;
+
+
+  onSelect(classification: number) {
+    this.selectedClass = classification;
+  }
 }
